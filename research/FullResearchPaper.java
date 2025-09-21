73d13c319ac897227d76d29d94467df10a36c5f3
Meghatania03 <141716710+Meghatania03@users.noreply.github.com>
2025-09-21 15:05:11 +0600

    added method in main

--- research/FullResearchPaper.java
+++ research/FullResearchPaper.java
@@ -4,4 +4,7 @@ public class FullResearchPaper implements ResearchPaper {
     public void view() {
         System.out.println("Viewing full research paper.");
     }
+    public void getAuthorName(){
+        System.out.println("Author: John Doe");
+    }
 }
