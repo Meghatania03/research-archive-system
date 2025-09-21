105a32557e383df482d3327c7e77f8d52dc1b250
Meghatania03 <141716710+Meghatania03@users.noreply.github.com>
2025-09-21 15:06:48 +0600

    added method in new branch

--- research/FullResearchPaper.java
+++ research/FullResearchPaper.java
@@ -4,4 +4,7 @@ public class FullResearchPaper implements ResearchPaper {
     public void view() {
         System.out.println("Viewing full research paper.");
     }
+    public void getPublicationDetails(){
+        System.out.println("Publication details of the research paper.");
+    }
 }
